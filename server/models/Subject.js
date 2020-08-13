be4b402ca7e49ab30f177e3eba0087875965aa35
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-08-13 08:06:30 -0700

    Subject model update

--- server/models/Subject.js
+++ server/models/Subject.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const subjectSchema = new Schema({
-  name: {
+  subject: {
     type: String,
     required: true,
     trim: true
