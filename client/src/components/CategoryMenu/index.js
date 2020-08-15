58aa2a67093aaa505dc3bafae7d38fbf51b5303e
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-08-15 16:54:16 -0700

    fixing the profile page and Category Menu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -4,28 +4,21 @@ import { QUERY_SUBJECTS, QUERY_OFFERINGS } from "../../utils/queries";
 import {UPDATE_SUBJECTS, UPDATE_CURRENT_SUBJECT} from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
 import { useDispatch, useSelector } from 'react-redux';
-
 function CategoryMenu() {
-
   const state = useSelector((state) => {
     return state
   });
   const dispatch = useDispatch();
-
   const { subjects } = state;
-  const { loading, data: categoryData } = useQuery(QUERY_OFFERINGS);
-
-
+  const { loading, data: categoryData } = useQuery(QUERY_SUBJECTS);
   useEffect(() => {
     //if categoryData exists or has changed from the response of useQuery, then run dispatch()
-
     if(categoryData) {
       //execute our dispatch function with our action object indicating the type of action and the data to set our state for subjects to
       dispatch({
         type: UPDATE_SUBJECTS,
         subjects: categoryData.subjects
       });
-
       categoryData.subjects.forEach(subject => {
         idbPromise('subjects', 'put', subject)
       }) 
@@ -38,15 +31,12 @@ function CategoryMenu() {
       })
     }
   }, [categoryData, loading, dispatch]);
-
-
   const handleClick = id => {
     dispatch({
       type: UPDATE_CURRENT_SUBJECT,
       currentSubject: id
     });
   };
-
   return (
     <div>
       <h2>Choose a Subject:</h2>
@@ -57,11 +47,10 @@ function CategoryMenu() {
             handleClick(item._id);
           }}
         >
-          {item.name}
+          {item.subject}
         </button>
       ))}
     </div>
   );
 }
-
 export default CategoryMenu;
