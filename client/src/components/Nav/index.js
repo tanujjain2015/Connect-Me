849749c307d670f6cba8da18d5dbc76b8837ba45
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-08-17 14:48:46 -0700

    Nav and HP Updates (in progress

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +8,31 @@ import Cart from '../Cart';
 
 
 function Nav() {
+
+  const [searchedSubjects, setSearchedSubjects] = useState({
+    visible: false
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if(searchedSubjects.visible == false) {
+      setSearchedSubjects({
+        visible: true
+      })
+    }
+      
+    // try {
+    //   // await allOfferings({
+    //   //   // variables: { id: user._id }
+    //   // })
+    //   await allOfferings
+    //   console.log(offerings, 'help')
+    //   const setSearchedSubjects = (searchedSubjects + 1);
+    // } catch (e) {
+    //   console.error(e);
+    // }
+  };
 
   function showNavigation() {
     
@@ -36,14 +61,14 @@ function Nav() {
       return (
           <div className="my-2 text-center">
 
-<button class="btn text-light my-2 my-sm-0" type="submit">Find A Tutor</button>
-<button class="btn text-light my-2 my-sm-0" type="submit">About Us</button>
+<button className="btn text-light my-2 my-sm-0" type="submit">Find A Tutor</button>
+<button className="btn text-light my-2 my-sm-0" type="submit">About Us</button>
         
           <Link to="/login">
-          <button class="btn text-light my-2 my-sm-0" type="submit">Login</button>
+          <button className="btn text-light my-2 my-sm-0" type="submit">Login</button>
           </Link>
         <Link to="/signup">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Join For Free</button>
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Join For Free</button>
         </Link>
         </div>
       );
@@ -58,7 +83,18 @@ function Nav() {
           Connect Me
         </Link>
       </h1>
-      <nav class="w-75">
+        <form className="form-inline ml-3" onSubmit={handleFormSubmit}>
+      {/* <form className="form-inline my-lg-0 position-absolute fixed-top mt-10 pl-5" onSubmit={async (event) => {
+        event.preventDefault();
+        await allOfferings;
+        console.log("hit")
+        setSearchedSubjects(searchedSubjects == 1);
+        console.log(setSearchedSubjects)
+      }}> */}
+      <input className="form-control mr-sm-2 w-75" type="search" placeholder="Search For An Offering" aria-label="Search" />
+      <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+       </form>
+      <nav className="">
         {showNavigation()}
         <Cart />
       </nav>
