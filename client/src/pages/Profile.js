83088bcb7e12be0d1956e1d6916c9e9d4d8e4752
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-08-17 11:16:48 -0700

    changes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -326,7 +326,7 @@ function Profile(props) {
                     } 
                     } type="submit">Update</button> */}
                     <button type="submit" className = "btn btn-light ml-auto" onClick={() => {setState({open: !state.open})}}><Link to ={{pathname: '/profileupdate',user }}>Edit Profile</Link></button>
-                    <button type="submit" className = "btn btn-light ml-auto"><Link to="/manageofferings">Manage my Offerings</Link></button>
+                    <button type="submit" className = "btn btn-light ml-auto"><Link to="/manageofferings">Manage Offerings</Link></button>
                     <button type="submit" className = "btn btn-light ml-auto"><Link to="/">Home</Link></button>
                     {/* <Button color="primary">Hello World</Button>
                     <Box component="div" display="inline">This is a sample box</Box> */}
