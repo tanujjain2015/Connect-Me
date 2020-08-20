3b815dee441597da2681d30e25897b4d43a1a635
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-08-20 12:57:10 -0700

    adding icons and changing the UI of OrderHistory

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -68,7 +68,34 @@ import {
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_ME } from "../utils/queries";
 
+//
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "../components/Card/Card.js";
+import CardBody from "../components/Card/CardBody.js";
+import CardHeader from "../components/Card/CardHeader.js";
+import CardFooter from "../components/Card/CardFooter.js";
+import Button from "../components/CustomButtons/Button.js";
+
+import { cardTitle } from "../assets/jss/material-kit-react.js";
+
 function OrderHistory() {
+
+  const styles = {
+    cardTitle,
+    textCenter: {
+      textAlign: "center",
+      // margin: "auto",
+      margin:"10px"
+    
+    },
+    textMuted: {
+      color: "#6c757d",
+    },
+  };
+  
+  const useStyles = makeStyles(styles);
+  const classes = useStyles();
+  //=============================================================
   const { data } = useQuery(QUERY_ME);
   let user;
 
@@ -92,9 +119,24 @@ function OrderHistory() {
             {user.orders.map((order) => (
               <div key={order._id} className="my-2">
                 <h3>{new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</h3>
+                <br/>
                 <div className="flex-row">
                   {order.offerings.map(({ _id, image, name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">
+                    // <div key={index} className="card px-1 py-1">
+                      // <Link to={`/offerings/${_id}`}>
+                      //   {/* <img
+                      //     alt={name}
+                      //     src={`/images/${image}`}
+                      //   /> */}
+                      //   <p>{name}</p>
+                      // </Link>
+                    //   <div>
+                    //     <span>${price}</span>
+                    //   </div>
+                    // </div>
+                    <div>
+                    <Card className={classes.textCenter} style={{ width: 20 + "em" }} key={index}>
+                      <CardHeader color="warning">
                       <Link to={`/offerings/${_id}`}>
                         {/* <img
                           alt={name}
@@ -102,10 +144,17 @@ function OrderHistory() {
                         /> */}
                         <p>{name}</p>
                       </Link>
-                      <div>
-                        <span>${price}</span>
-                      </div>
-                    </div>
+                      </CardHeader>
+                      <CardBody>
+                        <h4 className={classes.cardTitle}>
+                          ${price}
+                        </h4>
+    
+                      </CardBody>
+                    
+                    </Card>
+                  </div>
+              
                   ))}
                 </div>
               </div>
